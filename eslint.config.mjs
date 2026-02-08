9585ee4623dc61b14f266389729bc6a5e3724bcc
ashroff23 <ashroff2@charlotte.edu>
2026-02-08 18:14:38 -0500

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,26 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
+const baseConfig = {
+  plugins: { js },
+  extends: ["js/recommended"],
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+      ...globals.node,
+    },
+  },
+};
+
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    ...baseConfig,
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "script",
+    },
+  },
 ]);
