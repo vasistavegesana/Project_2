172be388ce73ea65864ddac91ef2f2ae04bf54f5
jsingh0802 <jsingh20@charlotte.edu>
2026-02-08 17:29:06 -0500

    Update template-processor.js

--- template-processor.js
+++ template-processor.js
@@ -1,33 +1,21 @@
 'use strict';
 
-// Constructor function that stores the template string
+// Constructor function
 function TemplateProcessor(template) {
-  // Save the template so it can be used later
   this.template = template;
 }
 
-// Method that fills in the template using values from a dictionary
-TemplateProcessor.prototype.fillIn = function (dictionary) {
+// Prototype method
+TemplateProcessor.prototype.fillIn = function fillIn(dictionary) {
+  const dict = dictionary || {};
 
-  // Replace all placeholders of the form {{key}} in the template
-  return this.template.replace(/{{(.*?)}}/g, function (match, key) {
-
-    // Check if the dictionary actually contains the key
-    if (Object.prototype.hasOwnProperty.call(dictionary, key)) {
-
-      // If the key exists, replace the placeholder with its value
-      return dictionary[key];
+  // Replace all occurrences of {{property}} where property is letters/numbers/underscore
+  return this.template.replace(/{{\s*([A-Za-z0-9_]+)\s*}}/g, function (_match, prop) {
+    // If property missing in dictionary, replace with empty string
+    if (Object.prototype.hasOwnProperty.call(dict, prop)) {
+      return String(dict[prop]);
     }
-
-    // If the key does not exist in the dictionary,
-    // replace the placeholder with an empty string
     return '';
   });
 };
-
-// Make TemplateProcessor available in both browser and Node environments
-if (typeof window !== 'undefined') {
-  window.TemplateProcessor = TemplateProcessor;
-} else if (typeof global !== 'undefined') {
-  global.TemplateProcessor = TemplateProcessor;
-}
+globalThis.TemplateProcessor = TemplateProcessor;
